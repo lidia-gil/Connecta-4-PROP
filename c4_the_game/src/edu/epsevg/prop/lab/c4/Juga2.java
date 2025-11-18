f4b84c6e67daa7d9fbdf03e16cb53157660ae584
lidia-gil <lidia.gil.martin@estudiantat.upc.edu>
2025-11-18 21:05:04 +0100

    Avanç i finalització de la implementació del MiniMax sense poda ni heuristica, fet en conjunt.
    Co-authored-by: danigarcia-05 <danigarcia-05@users.noreply.github.com>

--- c4_the_game/src/edu/epsevg/prop/lab/c4/Juga2.java
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/Juga2.java
@@ -95,8 +95,9 @@ public class Juga2 extends javax.swing.JFrame {
         //</editor-fold>
 
         // Definiu al vostre gust els jugadors a enfrontar.
-        Jugador p1 = new Manual();
-        //Jugador p1 = new Aleatori();
+       // Jugador p1 = new Manual();
+        Jugador p1 = new Jugador1(2);
+        //Jugador p2 = new Aleatori();
 
         Jugador p2 = new Profe(2,false);
         //Jugador p2 = new Manual();
---
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/Jugador1.java
@@ -0,0 +1,77 @@
+package edu.epsevg.prop.lab.c4;
+
+public class Jugador1
+  implements Jugador, IAuto
+{
+  private String nom;
+  private int profunditat;
+  
+  public Jugador1(int profunditat)
+  {
+    nom = "MiniMaxPlayer";
+    this.profunditat = profunditat;
+  }
+  
+  // hem de afegir per aqui al minimax
+  public int moviment(Tauler t, int color) {
+    int valor = Integer.MIN_VALUE;
+    int millorMoviment = -1;
+    for (int i = 0; i < t.getMida(); i++){
+      if (t.movpossible(i)){
+        Tauler taulerMov = new Tauler(t);
+        taulerMov.afegeix(i, color);
+        int prof = profunditat;
+        int candidat = MinValor(taulerMov, i, color*-1, prof-1);
+        if (valor < candidat || valor==Integer.MIN_VALUE){
+          valor = candidat;
+          millorMoviment = i;
+        } 
+      }    
+    }
+    return millorMoviment;
+  }
+  
+
+  public String nom()
+  {
+    return nom;
+  }
+
+
+  private int MinValor(Tauler t, int column, int color, int prof) {
+    if (t.solucio(column, color*-1) || !t.espotmoure() || prof==0){
+      //return evaluaEstat(t);
+      if (t.solucio(column, color*-1)) return Integer.MAX_VALUE;
+      if(!t.espotmoure()) return 0;
+      if (prof==0) return 0;
+    } 
+    int valor = Integer.MAX_VALUE;
+    for (int i = 0; i < t.getMida(); i++){
+      if (t.movpossible(i)){
+        Tauler taulerMov = new Tauler(t);
+        taulerMov.afegeix(i, color);
+        valor = Math.min(valor, MaxValor(taulerMov, i, color*-1, prof-1));
+      } 
+    } 
+    return valor;
+  }
+
+
+  private int MaxValor (Tauler t, int column, int color, int prof) {
+    if ((t.solucio(column, color*-1) || !t.espotmoure()) || prof==0){
+      //return evaluaEstat(t);
+      if (t.solucio(column, color*-1)) return Integer.MIN_VALUE;
+      if(!t.espotmoure()) return 0;
+      if (prof==0) return 0;
+    } 
+    int valor = Integer.MIN_VALUE;
+    for (int i = 0; i < t.getMida(); i++){
+      if (t.movpossible(i)){
+        Tauler taulerMov = new Tauler(t);
+        taulerMov.afegeix(i, color);
+        valor = Math.max(valor, MinValor(taulerMov, i, color*-1, prof-1));
+      } 
+    } 
+    return valor;
+  }
+}
