5f4ff2c6574885091662001e271d21547d883263
lidia-gil <lidia.gil.martin@estudiantat.upc.edu>
2025-11-23 17:13:34 +0100

    Creació de la classe HeuristicaC4 i neteja de repetició de codi, fet en conjunt.
    Co-authored-by: danigarcia-05 <danigarcia-05@users.noreply.github.com>

---
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/HeuristicaC4.java
@@ -0,0 +1,90 @@
+package edu.epsevg.prop.lab.c4;
+
+public class HeuristicaC4 {
+
+  // 1- Prioritzar columnes centrals
+  // 2- No continuar apilant fitxes en columnes on ja no tens hueco 
+  // 3- Prioritzar bloquejar doble amenaces del rival
+  // 4- Prioritzar crear dobles amenaces ( guanyar des de dues columnes diferents ) 
+  // 5- Prioritzar guanyar, no tapar al rival ( ja es té en compte en principi amb el minimax )
+  // 6- Idea feliç: intentar fer trampes pel rival ( posar una fitxa que li faci pensar que pot guanyar en un lloc, i després guanyar per un altre lloc )
+
+  private static int calculaHeuristica4(int pos[]) {
+    //0:buit; -1:rival; 1:jugador
+    int h = 0; 
+  
+    int buits = 0;
+    int rival = 0;
+    int jugador = 0;
+
+    for (int i = 0; i < 4; i++) {
+      if (pos[i] == 0) ++buits;
+      else if (pos[i] == -1) ++rival;
+      else ++jugador;
+    }
+    
+    // Jugador
+    if (jugador == 4) return 1000000;
+    if (jugador == 3 && buits == 1) return 100;
+    if (jugador == 2 && buits == 2) return 30; //30
+    if (jugador == 1 && buits == 3) return 1;
+
+    // Rival
+    if (rival == 4) return -1000000;
+    if (rival == 3 && buits == 1) return -100;
+    if (rival == 2 && buits == 2) return -30; //-20
+    if (rival == 1 && buits == 3) return -1;
+        
+    return h;
+  }
+  
+  private static int evaluaHVD(Tauler t, int fila, int col, int sumaFila, int sumaCol, int colorJugadorIni) {
+
+      int pos[] = new int[4];
+      for (int i = 0; i < 4; i++) {
+          if (colorJugadorIni == t.getColor(fila, col)) pos[i] = 1;
+          else if (colorJugadorIni*-1 == t.getColor(fila, col)) pos[i] = -1;
+          fila += sumaFila;
+          col += sumaCol;
+      }
+      return calculaHeuristica4(pos);
+  }
+
+  public static int evaluaEstat(Tauler t, int color, int colorJugadorIni) {
+    
+    int h = 0;
+    
+    //horitzontals
+    for (int fila = 0; fila < t.getMida(); fila++) {
+      for (int col = 0; col < t.getMida()-3; col++) {
+        h += evaluaHVD(t, fila, col, 0, 1, colorJugadorIni);
+        if (h >= 1000000) return h;
+      }  
+    }
+    
+    //verticals
+    for (int col = 0; col < t.getMida(); col++) {
+      for (int fila = 0; fila < t.getMida()-3; fila++) {
+        h += evaluaHVD(t, fila, col, 1, 0, colorJugadorIni);
+        if (h >= 1000000) return h;
+      }  
+    }
+
+    //diagonals adalt - dreta
+    for (int col = 0; col < t.getMida()-3; col++) {
+      for (int fila = 0; fila < t.getMida()-3; fila++) {
+        h += evaluaHVD(t, fila, col, 1, 1, colorJugadorIni);
+        if (h >= 1000000) return h;
+      }      
+    }
+
+    // diagonals adalt - esquerra
+    for (int col = 3; col < t.getMida(); col++) {
+      for (int fila = 0; fila < t.getMida()-3; fila++) {
+        h += evaluaHVD(t, fila, col, 1, -1, colorJugadorIni);
+        if (h >= 1000000) return h;
+      }      
+    }
+    return h;
+  }
+}
