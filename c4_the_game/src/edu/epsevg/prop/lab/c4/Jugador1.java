189cea4a2a1781b04f86d53a5c98e67730a5e146
lidia-gil <lidia.gil.martin@estudiantat.upc.edu>
2025-11-23 14:51:28 +0100

    Modificació de la puntuació de la heuristica després de realització de múltiples proves, fet en conjunt.
    Co-authored-by: danigarcia-05 <danigarcia-05@users.noreply.github.com>

--- c4_the_game/src/edu/epsevg/prop/lab/c4/Jugador1.java
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/Jugador1.java
@@ -91,45 +91,7 @@ public class Jugador1
     } 
     return valor;
   }
-/* 
-  private int evaluaEstat(Tauler t, int color) {
-    int h = 0;
-    for (int fila = 0; fila < t.getMida(); fila++) {
-      for (int col = 0; col < t.getMida(); col++) {
-        
-        int colorFitxa = t.getColor(fila, col);
 
-        if (colorFitxa == color) {
-          if (col==0 || col==7) h += 0;
-          else if (col==1 || col==6) h += 2;
-          else if (col==2 || col==5) h += 5;
-          else if (col==3 || col==4) h += 15;
-        } 
-        
-        if (colorFitxa != color){
-
-          int rival = color*-1;
-          if (col + 1 < (t.getMida()-1) && t.getColor(fila, col + 1) == rival && col != 0 ) { // mirar filesssss + prioritzar 3 fitxes del rival seguides
-
-            boolean foratEsq = (col - 1 >= 0 && t.getColor(fila, col - 1) == 0);
-
-            boolean foratDer = (col + 2 < t.getMida() && t.getColor(fila, col + 2) == 0);
-
-            if (foratEsq && foratDer) {
-              h -= 200;
-            }
-            
-            else if (foratEsq || foratDer) {
-              h -= 80;   
-            }
-          }
-        }
-      }
-    }
-    return h;
-  }
-}
-*/
 
 // fer classe auxiliar per fer la heuristica 
 
@@ -155,7 +117,6 @@ else {
 // 4- Prioritzar crear dobles amenaces ( guanyar des de dues columnes diferents ) 
 // 5- Prioritzar guanyar, no tapar al rival ( ja es té en compte en principi amb el minimax )
 // 6- Idea feliç: intentar fer trampes pel rival ( posar una fitxa que li faci pensar que pot guanyar en un lloc, i després guanyar per un altre lloc )
-
 
 
   private int calculaHeuristica4(int pos0, int pos1, int pos2, int pos3) {
@@ -184,19 +145,20 @@ else {
     // Jugador
     if (jugador == 4) return 1000000;
     if (jugador == 3 && buits == 1) return 100;
-    if (jugador == 2 && buits == 2) return 10;
+    if (jugador == 2 && buits == 2) return 30; //30
     if (jugador == 1 && buits == 3) return 1;
 
     // Rival
     if (rival == 4) return -1000000;
     if (rival == 3 && buits == 1) return -100;
-    if (rival == 2 && buits == 2) return -10;
+    if (rival == 2 && buits == 2) return -30; //-20
     if (rival == 1 && buits == 3) return -1;
         
     return h;
   }
   
   private int evaluaEstat(Tauler t, int color, int jugadorIni) {
+    
     int h = 0;
     
     //horitzontals
@@ -288,4 +250,187 @@ else {
     }
     return h;
   }
+   
+   /* 
+  private int calculaHeuristica4(int pos0, int pos1, int pos2, int pos3, int profunditatBuits) {
+    //0:buit; -1:rival; 1:jugador
+    int h = 0; 
+  
+    int buits = 0;
+    int rival = 0;
+    int jugador = 0;
+    
+    if(pos0==0) ++buits;  
+    if(pos1==0) ++buits;
+    if(pos2==0) ++buits;
+    if(pos3==0) ++buits;
+        
+    if(pos0==-1) ++rival;
+    if(pos1==-1) ++rival;
+    if(pos2==-1) ++rival;
+    if(pos3==-1) ++rival;
+    
+    if(pos0==1) ++jugador;
+    if(pos1==1) ++jugador;
+    if(pos2==1) ++jugador;
+    if(pos3==1) ++jugador;
+    
+   // Jugador
+    if (jugador == 4) return 1000000;
+    if (jugador == 3 && buits == 1) return (int)(100 / (1 + 0.5 * profunditatBuits));
+    if (jugador == 2 && buits == 2) return (int)(10 / (1 + 0.5 * profunditatBuits));
+    if (jugador == 1 && buits == 3) return (int)(1 / (1 + 0.5 * profunditatBuits));
+
+    // Rival
+    if (rival == 4) return -1000000;
+    if (rival == 3 && buits == 1) return (int)(-100 / (1 + 0.5 * profunditatBuits));
+    if (rival == 2 && buits == 2) return (int)(-10 / (1 + 0.5 * profunditatBuits));
+    if (rival == 1 && buits == 3) return (int)(-1 / (1 + 0.5 * profunditatBuits));
+
+    return h;
+  }
+
+  //pre: la casella (fila,columna) està buida
+  private int jugabilitatCasellaBuida(Tauler t, int fila, int columna) {
+    int buits = -1;
+    while (fila >= 0 && t.getColor(fila, columna) == 0) {
+      --fila;
+      ++buits;
+    }
+    return buits;
+  }
+  
+  private int evaluaEstat(Tauler t, int color, int jugadorIni) {
+    int h = 0;
+    
+    //horitzontals
+    for (int fila = 0; fila < t.getMida(); fila++) {
+      for (int col = 0; col < t.getMida()-3; col++) {
+          
+        int pos0=1, pos1=1, pos2=1, pos3=1;
+        int profunditatBuits = 0;;
+        
+        if (0 == t.getColor(fila, col)) {
+          pos0 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila, col);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila, col)) pos0 = -1;
+        
+        if (0 == t.getColor(fila, col+1)) {
+          pos1 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila, col+1);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila, col+1)) pos1 = -1;
+        
+        if (0 == t.getColor(fila, col+2)) {
+          pos2 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila, col+2);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila, col+2)) pos2 = -1;
+        
+        if (0 == t.getColor(fila, col+3)) {
+          pos3 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila, col+3);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila, col+3)) pos3 = -1;
+        
+        h += calculaHeuristica4(pos0, pos1, pos2, pos3, profunditatBuits);
+      }  
+    }
+    
+    //verticals
+    for (int col = 0; col < t.getMida(); col++) {
+      for (int fila = 0; fila < t.getMida()-3; fila++) {
+        
+        int pos0=0, pos1=0, pos2=0, pos3=0;
+        int profunditatBuits = 0;
+        
+        if (jugadorIni == t.getColor(fila, col)) pos0 = 1;
+        else if (jugadorIni*-1 == t.getColor(fila, col)) pos0 = -1;
+        
+        if (jugadorIni == t.getColor(fila+1, col)) pos1 = 1;
+        else if (jugadorIni*-1 == t.getColor(fila+1, col)) pos1 = -1;
+        
+        if (jugadorIni == t.getColor(fila+2, col)) pos2 = 1;
+        else if (jugadorIni*-1 == t.getColor(fila+2, col)) pos2 = -1;
+        
+        if (jugadorIni == t.getColor(fila+3, col)) pos3 = 1;
+        else if (jugadorIni*-1 == t.getColor(fila+3, col)) pos3 = -1;
+        
+        h += calculaHeuristica4(pos0, pos1, pos2, pos3, profunditatBuits);
+      }  
+    }
+
+    //diagonals adalt - dreta
+    for (int col = 0; col < t.getMida()-3; col++) {
+      for (int fila = 0; fila < t.getMida()-3; fila++) {
+        
+        int pos0=1, pos1=1, pos2=1, pos3=1;
+        int profunditatBuits = 0;
+        
+        if (0 == t.getColor(fila, col)) {
+          pos0 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila, col);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila, col)) pos0 = -1;
+        
+        if (0 == t.getColor(fila+1, col+1)) {
+          pos1 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila+1, col+1);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila+1, col+1)) pos1 = -1;
+        
+        if (0 == t.getColor(fila+2, col+2)) {
+          pos2 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila+2, col+2);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila+2, col+2)) pos2 = -1;
+        
+        if (0 == t.getColor(fila+3, col+3)) {
+          pos3 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila+3, col+3);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila+3, col+3)) pos3 = -1;
+        
+        h += calculaHeuristica4(pos0, pos1, pos2, pos3, profunditatBuits);
+      }      
+    }
+
+    // diagonals adalt - esquerra
+    for (int col = 3; col < t.getMida(); col++) {
+      for (int fila = 0; fila < t.getMida()-3; fila++) {
+        
+        int pos0=1, pos1=1, pos2=1, pos3=1;
+        int profunditatBuits = 0;
+        
+        if (0 == t.getColor(fila, col)) {
+          pos0 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila, col);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila, col)) pos0 = -1;
+        
+        if (0 == t.getColor(fila+1, col-1)) {
+          pos1 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila+1, col-1);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila+1, col-1)) pos1 = -1;
+        
+        if (0 == t.getColor(fila+2, col-2)) {
+          pos2 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila+2, col-2);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila+2, col-2)) pos2 = -1;
+        
+        if (0 == t.getColor(fila+3, col-3)) {
+          pos3 = 0;
+          profunditatBuits += jugabilitatCasellaBuida(t, fila+3, col-3);
+        }
+        else if (jugadorIni*-1 == t.getColor(fila+3, col-3)) pos3 = -1;
+        
+        h += calculaHeuristica4(pos0, pos1, pos2, pos3, profunditatBuits);
+      }      
+    }
+    return h;
+  }
+    */
 }
